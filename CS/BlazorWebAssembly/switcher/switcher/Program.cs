30c1ee4c6f4a0ae5bc5cea3ced8478775cafe85c
Elena Khamlyuk <80813840+khamlyuk@users.noreply.github.com>
2021-12-24 16:44:27 +0300

    Update program.cs and css (#4)
    * update blazor server
    
    * update wasm

---
+++ CS/BlazorWebAssembly/switcher/switcher/Program.cs
@@ -0,0 +1,16 @@
+﻿using switcher;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDevExpressBlazor();
+
+builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+
+await builder.Build().RunAsync();
