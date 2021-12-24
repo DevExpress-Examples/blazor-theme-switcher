f899f7dc7d12c06e134e0d17f09f3302e91f1c0b
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2021-12-24 16:21:41 +0300

    update blazor server

---
+++ CS/BlazorServer/switcher/switcher/Program.cs
@@ -0,0 +1,36 @@
+using switcher.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDevExpressBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
--- CS/BlazorWebAssembly/BlazorAppThemes/Program.cs
+++ CS/BlazorWebAssembly/BlazorAppThemes/Program.cs
@@ -1,23 +1,16 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using BlazorAppThemes;
 using Microsoft.AspNetCore.Components.Web;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace BlazorAppThemes {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
-            builder.Services.AddDevExpressBlazor();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<BlazorAppThemes.Data.WeatherForecastService>();
-            
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDevExpressBlazor();
+
+builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+
+await builder.Build().RunAsync();
