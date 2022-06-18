7bb6631807df3f6d260fd60df8dbeb56188d2afb
DevExpressExampleBot <fakeEmail>
2022-06-18 09:43:02 +0400

    Source auto update [skip ci]

--- CS/BlazorServer/switcher/switcher/Shared/ThemeItem.cs
+++ CS/BlazorServer/switcher/switcher/Shared/ThemeItem.cs
@@ -3,6 +3,7 @@ namespace switcher.Shared {
     public record ThemeItem(string Name, string[] StylesheetLinkUrl) {
         public static readonly ThemeItem Default = Create("default");
         public static ThemeItem Create(string name) {
+            
             if (Utils.DevExpressThemes.Contains(name))
                 return new ThemeItem(name, new[] { $"_content/DevExpress.Blazor.Themes/{name}.bs5.min.css" });
             return new ThemeItem(name, new[] { "_content/DevExpress.Blazor.Themes/bootstrap-external.bs5.min.css", $"css/switcher-resources/themes/{name}/bootstrap.min.css" });
